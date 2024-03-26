e281e41bb81ecad36b8fa92a147f106dd0678983
jonathanmrz <Jonathanmorozenviski@gmail.com>
2024-03-26 20:59:21 -0300

    ver1.00

---
+++ regilog/conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+$usuario - 'root';
+$senha - '';
+$database - 'login';
+$host - 'localhost';
+$mysqli - new mysqli($host,$usuario,$senha,$database);
+if ($mysqli -> error){
+    die("Falha ao conectar o banco de dados". $mysqli->error);
+}
