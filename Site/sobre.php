d245c294e31863db5b9ea6813f6396cd4724e2e6
JonathanMrz <149016977+jonathanMrz@users.noreply.github.com>
2024-04-29 17:19:46 -0300

    attt

--- Site/sobre.php
+++ Site/sobre.php
@@ -4,6 +4,7 @@
     <!-- Meta tags Obrigatórias -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS/Js -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
@@ -11,18 +12,21 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	<!-- Meu CSS/Js-->
-	
+  <link rel="stylesheet" href="front/.externo/index/docs.theme.min.css">
+	<link rel="stylesheet" href="front/.myfront/all.css">
 	<!-- Externo CSS/Js-->
+
+
 
     <title>Rs Mercearia</title>
   </head>
   <body>
 	<!-- Code start-->
 	
-	<!-- Menu start -->
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" style="size: 250px;" href="/Site/index.html">
-			<img src="/Site/front/img/logo.ico" width="50" height="50" class="d-inline-block align-top" alt="">
+		<!-- Menu start -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand" style="size: 250px;" href="index.php">
+			<img src="front/img/logo.ico" width="50" height="50" class="d-inline-block align-top" alt="">
 			<font size="6">Rs Mercearia</font>
 		</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
@@ -32,31 +36,101 @@
 		  <ul class="navbar-nav">
 			<li class="nav-item dropdown">
 			  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				<img src="/Site/front/img/categoria.ico" width="25" height="25" alt="Categorias"> Categorias de produto
+				<img src="front/img/categoria.ico" width="25" height="25" alt="Categorias"> Categorias de produto
 			  </a>
 			  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-				<a class="dropdown-item" href="/Site/html/pareciveis.html">Parecíveis</a>
-				<a class="dropdown-item" href="/Site/html/limpeza.html">Limpeza</a>
-				<a class="dropdown-item" href="/Site/html/papelaria.html">Papelaria</a>
-				<a class="dropdown-item" href="/Site/html/outros.html">Outros</a>
+				<a class="dropdown-item" href="pareciveis.php">Parecíveis</a>
+				<a class="dropdown-item" href="limpeza.php">Limpeza</a>
+				<a class="dropdown-item" href="papelaria.php">Papelaria</a>
+				<a class="dropdown-item" href="outros.php">Outros</a>
 			  </div>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="/Site/html/carrinho.html"><img src="/Site/front/img/carrinho.ico" width="20" height="20" alt="Categorias"> Carrinho</a>
+				<a class="nav-link" href="carrinho.php"><img src="front/img/carrinho.ico" width="20" height="20" alt="Categorias"> Carrinho</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="/Site/html/perfil.html"><img src="/Site/front/img/perfil.ico" width="20" height="20" alt="Categorias"> Perfil</a>
+				<a class="nav-link" href="login.php"><img src="front/img/perfil.ico" width="20" height="20" alt="Categorias"> Perfil</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="/Site/html/sobre.html"><img src="/Site/front/img/sobre.ico" width="20" height="20" alt="Categorias"> Sobre</a>
+				<a class="nav-link" href="sobre.php"><img src="front/img/sobre.ico" width="20" height="20" alt="Categorias"> Sobre</a>
 			</li>
 		  </ul>
 		</div>
 	  </nav>
 	  <main role="main">
-      <!-- Menu end -->
+    <!--Menu end-->
+<section class="section_all bg-light" id="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="section_title_all text-center">
+                            <h3 class="font-weight-bold">Welcome To <span class="text-custom">Lorem Ipsum</span></h3>
+                            <p class="section_subtitle mx-auto text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br/>Lorem Ipsum has been the industry's standard dummy text.</p>
+                            <div class="">
+                                <i class=""></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="row vertical_content_manage mt-5">
+                    <div class="col-lg-6">
+                        <div class="about_header_main mt-3">
+                            <div class="about_icon_box">
+                                <p class="text_custom font-weight-bold">Lorem Ipsum is simply dummy text</p>
+                            </div>
+                            <h4 class="about_heading text-capitalize font-weight-bold mt-4">Lorem Ipsum is simply dummy text of the printing industry.</h4>
+                            <p class="text-muted mt-3">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
 
+                            <p class="text-muted mt-3"> Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="img_about mt-3">
+                            <img src="https://i.ibb.co/qpz1hvM/About-us.jpg" alt="" class="img-fluid mx-auto d-block">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mt-3">
+                    <div class="col-lg-4">
+                        <div class="about_content_box_all mt-3">
+                            <div class="about_detail text-center">
+                                <div class="about_icon">
+                                    <i class="fas fa-pencil-alt"></i>
+                                </div>
+                                <h5 class="text-dark text-capitalize mt-3 font-weight-bold">Creative Design</h5>
+                                <p class="edu_desc mt-3 mb-0 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="about_content_box_all mt-3">
+                            <div class="about_detail text-center">
+                                <div class="about_icon">
+                                    <i class="fab fa-angellist"></i>
+                                </div>
+                                <h5 class="text-dark text-capitalize mt-3 font-weight-bold">We make Best Result</h5>
+                                <p class="edu_desc mb-0 mt-3 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="about_content_box_all mt-3">
+                            <div class="about_detail text-center">
+                                <div class="about_icon">
+                                    <i class="fas fa-paper-plane"></i>
+                                </div>
+                                <h5 class="text-dark text-capitalize mt-3 font-weight-bold">best platform </h5>
+                                <p class="edu_desc mb-0 mt-3 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
 
 
